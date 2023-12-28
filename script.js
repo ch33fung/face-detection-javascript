cd65a3e344f321f00e41bd425a0d20d535057c5c
cheefung <ch33fung@gmail.com>
2023-12-28 11:17:17 +0800

    expressions

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ Promise.all([
 
 function startVideo() {
   navigator.getUserMedia(
-    { video: true,
-      audio: false },
+    { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
   )
@@ -19,17 +18,21 @@ function startVideo() {
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
+
   const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+    faceapi.matchDimensions(canvas, displaySize)
+
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new 
-    faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
-    .withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, 
-    displaySize)
+
+    const detections = await faceapi.detectAllFaces(video, new faceapi
+      .TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
